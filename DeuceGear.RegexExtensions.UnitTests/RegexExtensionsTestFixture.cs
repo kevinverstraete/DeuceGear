d220ca5d183f1b17ee1f22ee7a05a37396f16f1b
kevinverstraete <verstraete_kevin@telenet.be>
2018-02-24 20:41:24 +0100

    Regex Extensions

---
+++ DeuceGear.RegexExtensions.UnitTests/RegexExtensionsTestFixture.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text.RegularExpressions;
+
+namespace DeuceGear.RegexExtensions.UnitTests
+{
+    [TestFixture]
+    public class RegexExtensionsTestFixture
+    {
+        private const string phoneNumbers = "050/12.34.56 051/23.45.67 052/15.97.53 053/98.76.54";
+
+        [Test]
+        public void RegexExtensionsTestNullObjectThrowsError()
+        {
+            // arrange
+            Match match = null;
+
+            // act
+            var result = Assert.Throws<ArgumentNullException>(() => match.GetGroupValue("bob"));
+
+            // assert
+            Assert.That(result.ParamName, Is.EqualTo("match"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void RegexExtensionsTestNoNameThrowsError(string name)
+        {
+            // arrange
+            var pattern = @"(\d{3})/(\d{2}.\d{2}.\d{2})"; ;
+            var matches = Regex.Matches(phoneNumbers, pattern);
+
+            // act
+            var result = Assert.Throws<ArgumentNullException>(() => matches[0].GetGroupValue(name));
+
+            // assert
+            Assert.That(result.ParamName, Is.EqualTo("name"));
+        }
+
+        [Test]
+        public void RegexExtensionsTestGetGroupByName()
+        {
+            // arrange
+            var pattern = @"(?<area>\d{3})/(?<number>\d{2}.\d{2}.\d{2})"; ;
+            var matches = Regex.Matches(phoneNumbers, pattern);
+
+            // act/assert
+            RegexExtensionsTestGetGroupByNameSingle(matches[0], "050", "12.34.56");
+            RegexExtensionsTestGetGroupByNameSingle(matches[1], "051", "23.45.67");
+            RegexExtensionsTestGetGroupByNameSingle(matches[2], "052", "15.97.53");
+            RegexExtensionsTestGetGroupByNameSingle(matches[3], "053", "98.76.54");
+        }
+
+        private void RegexExtensionsTestGetGroupByNameSingle(Match match, string areaExpectation, string numberExpectation)
+        {
+            // act
+            var areaValue = match.GetGroupValue("area");
+            var numberValue = match.GetGroupValue("number");
+            var emptyValue = match.GetGroupValue("bob");
+
+            // assert
+            Assert.That(areaValue, Is.EqualTo(areaExpectation));
+            Assert.That(numberValue, Is.EqualTo(numberExpectation));
+            Assert.That(emptyValue, Is.EqualTo(string.Empty));
+        }
+    }
+}
---
+++ DeuceGear.RegexExtensions/RegexExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DeuceGear
+{
+    /// <summary>
+    /// Extension Methods for regularExpressions
+    /// </summary>
+    public static class RegexExtensions
+    {
+        /// <summary>
+        /// Get a specific group value based on the name.
+        /// </summary>
+        /// <param name="match">match to search in</param>
+        /// <param name="name">group name to search for</param>
+        /// <returns>
+        /// Value of the given group in the <c>Match</c>, if there is one
+        /// </returns>
+        public static string GetGroupValue(this Match match, string name)
+        {
+            if (match == null)
+                throw new ArgumentNullException("match");
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("name");
+
+            var group = match.Groups[name];
+            if (group == null)
+                return null;
+            return group.Value;
+        }
+    }
+}
