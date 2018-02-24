5d1a9ae8c162247988e7c0b5b3c143f593b01d20
kevinverstraete <verstraete_kevin@telenet.be>
2018-02-24 20:23:06 +0100

    String Extensions

---
+++ DeuceGear.StringExtensions.UnitTests/StringExtensionsTestFixture.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DeuceGear.StringExtensions.UnitTests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class StringExtensionsTestFixture
+    {
+        [Test]
+        // zero length case
+        [TestCase(null, 0, "")]
+        [TestCase("", 0, "")]
+        [TestCase("a", 0, "")]
+        [TestCase("ab", 0, "")]
+        [TestCase("abc", 0, "")]
+        [TestCase("abcd", 0, "")]
+        // negative length case
+        [TestCase(null, -1, "")]
+        [TestCase("", -1, "")]
+        [TestCase("a", -1, "")]
+        [TestCase("ab", -1, "")]
+        [TestCase("abc", -1, "")]
+        [TestCase("abcd", -1, "")]
+        // correct length case
+        [TestCase(null, 2, "")]
+        [TestCase("", 2, "")]
+        [TestCase("a", 2, "a")]
+        [TestCase("ab", 2, "ab")]
+        [TestCase("abc", 2, "bc")]
+        [TestCase("abcd", 2, "cd")]
+        public void StringExtensionsTestRight(string value, int length, string expectation)
+        {
+            // act + arrange
+            var result = value.Right(length);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expectation));
+        }
+
+        [Test]
+        // zero length case
+        [TestCase(null, 0, "")]
+        [TestCase("", 0, "")]
+        [TestCase("a", 0, "")]
+        [TestCase("ab", 0, "")]
+        [TestCase("abc", 0, "")]
+        [TestCase("abcd", 0, "")]
+        // negative length case
+        [TestCase(null, -1, "")]
+        [TestCase("", -1, "")]
+        [TestCase("a", -1, "")]
+        [TestCase("ab", -1, "")]
+        [TestCase("abc", -1, "")]
+        [TestCase("abcd", -1, "")]
+        // correct length case
+        [TestCase(null, 2, "")]
+        [TestCase("", 2, "")]
+        [TestCase("a", 2, "a")]
+        [TestCase("ab", 2, "ab")]
+        [TestCase("abc", 2, "ab")]
+        [TestCase("abcd", 2, "ab")]
+        public void StringExtensionsTestLeft(string value, int length, string expectation)
+        {
+            // act + arrange
+            var result = value.Left(length);
+
+            // assert
+            Assert.That(result, Is.EqualTo(expectation));
+        }
+    }
+}
